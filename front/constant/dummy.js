332666d6a1ad872bb63408ddd31eebd254e9d5c6
Gred <jihong.park.dev@gmail.com>
2020-05-22 19:10:47 +0900

    component 분리

---
+++ front/constant/dummy.js
@@ -0,0 +1,14 @@
+export const dummy = {
+    isLoggedIn: true,
+    imagePaths: [],
+    mainPosts: [
+        {
+            User: {
+                id: 1,
+                nickname: "gred",
+            },
+            content: "first message",
+            img: "/image/dummy_image.jpg",
+        },
+    ],
+};
